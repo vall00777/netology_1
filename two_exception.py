4a576a11fbd38710569139497ae26033c21a4d44
Валерий <36442895+vall00777@users.noreply.github.com>
2018-12-02 19:19:57 +0300

    Update two_exception.py

--- two_exception.py
+++ two_exception.py
@@ -1,13 +1,17 @@
-znak, a, b = input().split()
 try:
-     assert float(a) > 0 and float(b) > 0
-except AssertionError:
-     print('Число меньше 0')
-if znak == '+':
-     print(float(a)+float(b))
-elif znak == '-':
-     print(float(a)-float(b))
-elif znak == '*':
-     print(float(a)*float(b))
-elif znak == '/':
-     print(float(a)/float(b))
+     znak, a, b = input().split()
+except ValueError:
+     print("Неверный ввод")
+else:
+     try:
+          assert float(a) > 0 and float(b) > 0
+     except AssertionError:
+          print('Число меньше 0')
+     if znak == '+':
+          print(float(a)+float(b))
+     elif znak == '-':
+          print(float(a)-float(b))
+     elif znak == '*':
+          print(float(a)*float(b))
+     elif znak == '/':
+          print(float(a)/float(b))
